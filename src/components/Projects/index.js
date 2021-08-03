84041c2ec5ad86ec5152b432c000bf0feada85ce
Sabarinath049 <tejachillapalli@gmail.com>
2021-08-03 15:28:22 +0530

    changed whole project card and created new card

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -17,8 +17,8 @@ const Projects = () => {
                 <div className={`projects-main-container ${mode}`}>
                     <div className="project-container">
                         <div className="project-heading-container">
-                            <h1 className="projects-heading">Projects</h1>
-                            <p className="projects-description">My projects makes use of vast variety of latest technology tools. </p>
+                            <h1 className="projects-heading clearmargin">Projects</h1>
+                            <p className="projects-description clearmargin">My projects makes use of vast variety of latest technology tools. </p>
                         </div>
                         <div className="project-image-container">
                             <h1>Hello</h1>
@@ -27,81 +27,153 @@ const Projects = () => {
                     <div className="projects-list-container">
                         <div className={`${cardMode}`}>
                             <div className="heading-container">
-                                <h1 className="project-item-heading">Appointments App</h1>
-                                <FontAwesomeIcon icon={faLink} className="link-item"/>
+                                <h1 className="project-item-heading clearmargin">Appointments App</h1>
+                                <FontAwesomeIcon icon={faLink} className="link-item clearmargin"/>
                             </div>
-                            <div className="technology-container">
-                                <AiFillHtml5 className="technology-item html" />
-                                <DiCss3 className="technology-item css" />
-                                <DiJavascript1 className="technology-item js" />
-                                <DiReact className="technology-item react" />
+                            <div className="technology-container clearmargin">
+                                <div className="div">
+                                    <h5 className="text">Html</h5>
+                                    <AiFillHtml5 className="technology-item html" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">CSS</h5>
+                                    <DiCss3 className="technology-item css" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">Javascript</h5>
+                                    <DiJavascript1 className="technology-item js" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">React</h5>
+                                    <DiReact className="technology-item react" />
+                                </div>
                             </div>
-                            <p className="project-description">A web App that store all the appointments at one place, loaded with new features such as starring, filternig.</p>
+                            <p className="project-description clearmargin">A web App that store all the appointments at one place, loaded with new features such as starring, filternig.</p>
                         </div>
                         <div className={`${cardMode}`}>
                             <div className="heading-container">
-                                <h1 className="project-item-heading">Appointments App</h1>
-                                <FontAwesomeIcon icon={faLink} className="link-item"/>
+                                <h1 className="project-item-heading clearmargin">Appointments App</h1>
+                                <FontAwesomeIcon icon={faLink} className="link-item clearmargin"/>
                             </div>
-                            <div className="technology-container">
-                                <AiFillHtml5 className="technology-item html" />
-                                <DiCss3 className="technology-item css" />
-                                <DiJavascript1 className="technology-item js" />
-                                <DiReact className="technology-item react" />
+                            <div className="technology-container clearmargin">
+                                <div className="div">
+                                    <h5 className="text">Html</h5>
+                                    <AiFillHtml5 className="technology-item html" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">CSS</h5>
+                                    <DiCss3 className="technology-item css" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">Javascript</h5>
+                                    <DiJavascript1 className="technology-item js" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">React</h5>
+                                    <DiReact className="technology-item react" />
+                                </div>
                             </div>
-                            <p className="project-description">A web App that store all the appointments at one place, loaded with new features such as starring, filternig.</p>
+                            <p className="project-description clearmargin">A web App that store all the appointments at one place, loaded with new features such as starring, filternig.</p>
                         </div>
                         <div className={`${cardMode}`}>
                             <div className="heading-container">
-                                <h1 className="project-item-heading">Appointments App</h1>
-                                <FontAwesomeIcon icon={faLink} className="link-item"/>
+                                <h1 className="project-item-heading clearmargin">Appointments App</h1>
+                                <FontAwesomeIcon icon={faLink} className="link-item clearmargin"/>
                             </div>
-                            <div className="technology-container">
-                                <AiFillHtml5 className="technology-item html" />
-                                <DiCss3 className="technology-item css" />
-                                <DiJavascript1 className="technology-item js" />
-                                <DiReact className="technology-item react" />
+                            <div className="technology-container clearmargin">
+                                <div className="div">
+                                    <h5 className="text">Html</h5>
+                                    <AiFillHtml5 className="technology-item html" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">CSS</h5>
+                                    <DiCss3 className="technology-item css" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">Javascript</h5>
+                                    <DiJavascript1 className="technology-item js" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">React</h5>
+                                    <DiReact className="technology-item react" />
+                                </div>
                             </div>
-                            <p className="project-description">A web App that store all the appointments at one place, loaded with new features such as starring, filternig.</p>
+                            <p className="project-description clearmargin">A web App that store all the appointments at one place, loaded with new features such as starring, filternig.</p>
                         </div>
                         <div className={`${cardMode}`}>
                             <div className="heading-container">
-                                <h1 className="project-item-heading">Appointments App</h1>
-                                <FontAwesomeIcon icon={faLink} className="link-item"/>
+                                <h1 className="project-item-heading clearmargin">Appointments App</h1>
+                                <FontAwesomeIcon icon={faLink} className="link-item clearmargin"/>
                             </div>
-                            <div className="technology-container">
-                                <AiFillHtml5 className="technology-item html" />
-                                <DiCss3 className="technology-item css" />
-                                <DiJavascript1 className="technology-item js" />
-                                <DiReact className="technology-item react" />
+                            <div className="technology-container clearmargin">
+                                <div className="div">
+                                    <h5 className="text">Html</h5>
+                                    <AiFillHtml5 className="technology-item html" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">CSS</h5>
+                                    <DiCss3 className="technology-item css" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">Javascript</h5>
+                                    <DiJavascript1 className="technology-item js" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">React</h5>
+                                    <DiReact className="technology-item react" />
+                                </div>
                             </div>
-                            <p className="project-description">A web App that store all the appointments at one place, loaded with new features such as starring, filternig.</p>
+                            <p className="project-description clearmargin">A web App that store all the appointments at one place, loaded with new features such as starring, filternig.</p>
                         </div>
                         <div className={`${cardMode}`}>
                             <div className="heading-container">
-                                <h1 className="project-item-heading">Appointments App</h1>
-                                <FontAwesomeIcon icon={faLink} className="link-item"/>
+                                <h1 className="project-item-heading clearmargin">Appointments App</h1>
+                                <FontAwesomeIcon icon={faLink} className="link-item clearmargin"/>
                             </div>
-                            <div className="technology-container">
-                                <AiFillHtml5 className="technology-item html" />
-                                <DiCss3 className="technology-item css" />
-                                <DiJavascript1 className="technology-item js" />
-                                <DiReact className="technology-item react" />
+                            <div className="technology-container clearmargin">
+                                <div className="div">
+                                    <h5 className="text">Html</h5>
+                                    <AiFillHtml5 className="technology-item html" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">CSS</h5>
+                                    <DiCss3 className="technology-item css" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">Javascript</h5>
+                                    <DiJavascript1 className="technology-item js" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">React</h5>
+                                    <DiReact className="technology-item react" />
+                                </div>
                             </div>
-                            <p className="project-description">A web App that store all the appointments at one place, loaded with new features such as starring, filternig.</p>
+                            <p className="project-description clearmargin">A web App that store all the appointments at one place, loaded with new features such as starring, filternig.</p>
                         </div>
                         <div className={`${cardMode}`}>
                             <div className="heading-container">
-                                <h1 className="project-item-heading">Appointments App</h1>
-                                <FontAwesomeIcon icon={faLink} className="link-item"/>
+                                <h1 className="project-item-heading clearmargin">Appointments App</h1>
+                                <FontAwesomeIcon icon={faLink} className="link-item clearmargin"/>
                             </div>
-                            <div className="technology-container">
-                                <AiFillHtml5 className="technology-item html" />
-                                <DiCss3 className="technology-item css" />
-                                <DiJavascript1 className="technology-item js" />
-                                <DiReact className="technology-item react" />
+                            <div className="technology-container clearmargin">
+                                <div className="div">
+                                    <h5 className="text">Html</h5>
+                                    <AiFillHtml5 className="technology-item html" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">CSS</h5>
+                                    <DiCss3 className="technology-item css" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">Javascript</h5>
+                                    <DiJavascript1 className="technology-item js" />
+                                </div>
+                                <div className="div">
+                                    <h5 className="text">React</h5>
+                                    <DiReact className="technology-item react" />
+                                </div>
                             </div>
-                            <p className="project-description">A web App that store all the appointments at one place, loaded with new features such as starring, filternig.</p>
+                            <p className="project-description clearmargin">A web App that store all the appointments at one place, loaded with new features such as starring, filternig.</p>
                         </div>
                     </div>
                 </div>
