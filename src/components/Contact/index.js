dde347fbd567d2c7a0d2baa0c18ddb44502813cc
Sabarinath049 <tejachillapalli@gmail.com>
2021-08-03 15:27:28 +0530

    implemented hover on technologies

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -17,19 +17,31 @@ const Contact = () => {
                                 <h1>Image</h1>
                             </div>
                             <div className='contact-details-container'>
-                                <h1 className="contact-heading">Contact Me</h1>
-                                <p className="contact-description setlineheight">I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with MERN Development.</p>
-                                <div className="social-tech-container">
-                                    <AiFillGithub  className="icon-item" />
-                                    <AiFillLinkedin className="icon-item linkedin" />
-                                    <AiFillGoogleCircle className="icon-item google" />
-                                    <AiFillInstagram className="icon-item instagram" />
+                                <h1 className="contact-heading clearmargin">Contact Me</h1>
+                                <p className="contact-description clearmargin">I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with MERN Development.</p>
+                                <div className="social-tech-container clearmargin">
+                                    <div className="div">
+                                        <h5 className="text">Github</h5>
+                                        <AiFillGithub  className="icon-item" />
+                                    </div>
+                                    <div className="div">
+                                        <h5 className="text">Linkedin</h5>
+                                        <AiFillLinkedin className="icon-item linkedin" />
+                                    </div>
+                                    <div className="div">
+                                        <h5 className="text">Google</h5>
+                                        <AiFillGoogleCircle className="icon-item google" />
+                                    </div>
+                                    <div className="div">
+                                        <h5 className="text">Instagram</h5>
+                                        <AiFillInstagram className="icon-item instagram" />
+                                    </div>
                                 </div>
-                                <a href="mailto:tejachillapalli@gmail.com"><button type="button" className={`mail-button ${buttonMode}`}>Drop a mail</button></a>
-                                <h1 className="contact-heading setlineheight">Address</h1>
-                                <p className="contact-description setlineheight">Guntur, AndhraPradesh - 522001</p>
-                                <h1 className="contact-heading setlineheight">Phone Number</h1>
-                                <p className="contact-description setlineheight">+91 8919974741</p>
+                                <a href="mailto:tejachillapalli@gmail.com"><button type="button" className={`mail-button clearmargin ${buttonMode}`}>Drop a mail</button></a>
+                                <h1 className="contact-heading clearmargin">Address</h1>
+                                <p className="contact-description clearmargin">Guntur, AndhraPradesh - 522001</p>
+                                <h1 className="contact-heading clearmargin">Phone Number</h1>
+                                <p className="contact-description clearmargin">+91 8919974741</p>
                             </div>
                         </div>
                     )
