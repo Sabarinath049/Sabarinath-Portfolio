56f236366e973fe75c7e188717d6b094a321341b
Sabarinath049 <tejachillapalli@gmail.com>
2021-08-12 12:21:10 +0530

    added 2 projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,5 +1,5 @@
 import ModeContext from '../../Context/index'
-import {AiOutlineLink} from 'react-icons/ai'
+import {AiOutlineLink, AiFillGithub} from 'react-icons/ai'
 import AddressImg from './AddressImg'
 import './index.css'
 
@@ -10,6 +10,7 @@ const Projects = () => {
             const {activeMode} = value
             const mode = activeMode ? "dark" : "light"
             const cardMode = mode === 'dark' ? 'project-item-dark' : 'project-item-light'
+            const projectIconMode = activeMode === 'dark' ? 'dark-icon' : 'light-icon'
             const renderProjects = () => {
                 return(
                 <div className={`projects-main-container ${mode}`}>
@@ -25,8 +26,13 @@ const Projects = () => {
                     <div className="project-list-container">
                         <div className={`${cardMode}`}>
                             <div className="card-heading-container">
-                                <h3 className="project-card-heading">Appointment</h3>
-                                <AiOutlineLink className="link-item" />
+                                <h3 className="project-card-heading">NxtTrendz App</h3>
+                                <a className="anchor" href="https://sabarinathnxttrendz.netlify.app" target="_blank" rel="noreferrer">
+                                    <AiOutlineLink className={`link-item ${mode}`} />
+                                </a>
+                                <a className="anchor" href="https://github.com/Sabarinath049/NtxTrendz-Application" target="_blank" rel="noreferrer">
+                                    <AiFillGithub className={`link-item ${mode}`} />
+                                </a>
                             </div>
                             <div className="tech-names">
                                 <p className="tech-item html">HTML</p>
@@ -34,20 +40,24 @@ const Projects = () => {
                                 <p className="tech-item js">Javascript</p>
                                 <p className="tech-item react">React</p>
                             </div>
-                            <p className="project-card-description">A web App that store all the appointments at one place, loaded with new features such as starring, filternig.</p>
+                            <p className="project-card-description">It is an Ecommerce App that is used to add products into cart and checkout accordingly.</p>
                         </div>
                         <div className={`${cardMode}`}>
                             <div className="card-heading-container">
-                                <h3 className="project-card-heading">Appointment</h3>
-                                <AiOutlineLink className="link-item" />
+                                <h3 className="project-card-heading">Git Search</h3>
+                                <a className="anchor" href="https://sabarinathgithubsearch.netlify.app" target="_blank" rel="noreferrer">
+                                    <AiOutlineLink className={`link-item ${mode}`} />
+                                </a>
+                                <a className="anchor" href="https://github.com/Sabarinath049/GitSearch" target="_blank" rel="noreferrer">
+                                    <AiFillGithub className={`link-item ${mode}`} />
+                                </a>
                             </div>
                             <div className="tech-names">
                                 <p className="tech-item html">HTML</p>
                                 <p className="tech-item css">CSS</p>
                                 <p className="tech-item js">Javascript</p>
-                                <p className="tech-item react">React</p>
                             </div>
-                            <p className="project-card-description">A web App that store all the appointments at one place, loaded with new features such as starring, filternig.</p>
+                            <p className="project-card-description">We can search peeps who are present in git and get their details, repos, repo-stats</p>
                         </div>
                         <div className={`${cardMode}`}>
                             <div className="card-heading-container">
